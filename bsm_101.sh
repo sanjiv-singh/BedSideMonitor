4a9e0e87a24921ff17216d8be19fc90d5fcb0d61
Sanjiv Singh <sk.sanjiv@gmail.com>
2022-10-20 18:25:27 +0530

    scripts for running device simulators in parallel

---
+++ bsm_101.sh
@@ -0,0 +1,1 @@
+python BedSideMonitor.py -e a1mtq1xieuq18l-ats.iot.us-east-1.amazonaws.com --r certs/AmazonRootCA1.pem -d BSM_G101 -c certs/BSM_G101_device.pem -k certs/BSM_G101_private.key -id bsm_g101 -t iot/bsm
---
+++ bsm_102.sh
@@ -0,0 +1,1 @@
+python BedSideMonitor.py -e a1mtq1xieuq18l-ats.iot.us-east-1.amazonaws.com --r certs/AmazonRootCA1.pem -d BSM_G102 -c certs/BSM_G102_device.pem -k certs/BSM_G102_private.key -id bsm_g102 -t iot/bsm

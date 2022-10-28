a7ec857f1dcebab2a377ff942945215300491e46
Sanjiv Singh <sk.sanjiv@gmail.com>
2022-10-29 00:27:45 +0530

    ahange timestamp by a micro sec to avoid duplicate keys

--- RawDataModel.py
+++ RawDataModel.py
@@ -1,3 +1,4 @@
+from itertools import count
 from os import times
 from Database import Database
 from datetime import datetime, timedelta
@@ -12,19 +13,20 @@ class RawDataModel:
 
     def aggregate_by_datatype_and_minute(self, device_id, start_timestamp, end_timestamp):
         aggregated_data = []
+        count = 0
         for data_type in self._data_types:
             start_time = datetime.strptime(start_timestamp, '%Y-%m-%d %H:%M:%S.%f')
             time = start_time
             end_time = datetime.strptime(end_timestamp, '%Y-%m-%d %H:%M:%S.%f')
             while time <= end_time:
                 timestamp = time.strftime('%Y-%m-%d %H:%M:%S.%f')
-                print(timestamp)
                 time += timedelta(seconds=60)
                 timerange = (timestamp, time.strftime('%Y-%m-%d %H:%M:%S.%f'))
-                aggregated_data.append(self.aggregate(device_id, timerange, data_type))
+                aggregated_data.append(self.aggregate(device_id, timerange, data_type, count))
+                count += 1
         return aggregated_data
 
-    def aggregate(self, device_id, timerange, datatype):
+    def aggregate(self, device_id, timerange, datatype, count):
         data = self._db.query_range(device_id, timerange, datatype=datatype)
         min, max, sum = (9999, -9999, 0)
         for record in data:
@@ -35,9 +37,11 @@ class RawDataModel:
                 max = value
             sum += value
         avg = 1.0*sum/(1.0*len(data))
+        timestamp = datetime.strptime(timerange[0], '%Y-%m-%d %H:%M:%S.%f')
+        timestamp += timedelta(microseconds=count)  # Incrementing stored timestamp my a microsec to avoid duplicate keys
         return {'deviceid': device_id,
                 'datatype': datatype,
-                'timestamp': timerange[0],
+                'timestamp': timestamp.strftime('%Y-%m-%d %H:%M:%S.%f'),
                 'min': min,
                 'max': max,
                 'avg': avg

44a76fe9e7bee197c75a82e89a1f6f9b93509b61
Sanjiv Singh <sk.sanjiv@gmail.com>
2022-10-28 20:53:15 +0530

    Impl of DynamoDB query API

--- Database.py
+++ Database.py
@@ -1,170 +1,56 @@
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
-from boto3.dynamodb.types import DYNAMODB_CONTEXT
-
-dynamodb = boto3.resource("dynamodb")
+from botocore.exceptions import ClientError
 
 
-class DynamoDB:
-    def __init__(self, table_name):
-        self.table_name = table_name
-        self.table = dynamodb.Table(self.table_name)
+class Database:
+	# Initialization, mapping each instance to a specific table, and storing the partition and sort key names
+	def __init__(self, table_name, partition_key, sort_key):
+		self._dynamodb = boto3.resource('dynamodb')
+		self._table = self._dynamodb.Table(table_name)
+		self._partition_key = partition_key
+		self._sort_key = sort_key
 
-    def read_item(self, pk_name, pk_value):
-        """
-        Return item read by primary key.
-        """
-        response = self.table.get_item(Key={pk_name: pk_value})
-        return response
+	# To get exact single data based on partition and sort attribute values
+	def query_exact(self, partition_value, sort_value):
+		response = self._table.get_item(
+			Key={
+				'deviceid': partition_value,
+				'timestamp': sort_value
+			}
+		)
+		return response.get('Item')
+	
+	# To get data based on partition key value
+	def query(self, partition_value, filter_key=None, filter_value=None):
+		query_params = {}
+		query_params['KeyConditionExpression'] = Key('deviceid').eq(partition_value)
+		if filter_key:
+			query_params['FilterExpression'] = Attr(filter_key).eq(filter_value)
+		response = self._table.query(**query_params)
+		return response.get('Items')
 
-    def add_item(self, col_dict):
-        """
-        Add one item (row) to table. col_dict is a dictionary {col_name: value}.
-        """
-        response = self.table.put_item(Item=col_dict)
-        return response
+	# To get data based on partition attribute value and comparison with sort attribute range
+	def query_cmp(self, partition_value, cmp_op='lt', cmp_value='0'):
+		if cmp_op not in ['eq', 'lt', 'gt', 'lte', 'gte']:
+			raise Exception(f'{cmp_op}: Not a supported comparison operator.')
+		response = self._table.query(
+			KeyConditionExpression=eval(f'Key("deviceid").eq("{partition_value}") & Key("timestamp").{cmp_op}("{cmp_value}")')
+		)
+		return response.get('Items')
 
-    def update_item(self, pk_name, pk_value, col_dict):
-        """
-        update one item (row) to table. col_dict is a dictionary {col_name: value}.
-        """
+	# To get a continuous range of data based on partition attribute value and sort attribute range
+	def query_range(self, partition_value, sort_range, filter_key=None, filter_value=None):
+		query_params = {}
+		query_params['KeyConditionExpression'] = Key('deviceid').eq(partition_value) & \
+	    		Key('timestamp').between(sort_range[0], sort_range[1])
+		if filter_key:
+			query_params['FilterExpression'] = Attr(filter_key).eq(filter_value)
+		response = self._table.query(**query_params)
+		return response.get('Items')
 
-        update_expression = 'SET {}'.format(','.join(f'#{k}=:{k}' for k in col_dict))
-        expression_attribute_values = {f':{k}': v for k, v in col_dict.items()}
-        expression_attribute_names = {f'#{k}': k for k in col_dict}
+	# Inserts (or overwrites) a single item based on already formed item dict
+	def put_item(self, item):
+		pass
 
-        response = self.table.update_item(
-            Key={'{}'.format(pk_name): pk_value},
-            UpdateExpression=update_expression,
-            ExpressionAttributeValues=expression_attribute_values,
-            ExpressionAttributeNames=expression_attribute_names,
-            ReturnValues='UPDATED_NEW',
-        )
-
-        return response
-
-    def delete_item(self, pk_name, pk_value):
-        """
-        Delete an item (row) in table from its primary key.
-        """
-        return self.table.delete_item(Key={pk_name: pk_value})
-
-    def query(self, filter_key=None, filter_value=None):
-        """
-        Perform a query operation on the table.
-        Can specify filter_key (col name) and its value to be filtered.
-        """
-        if filter_key and filter_value:
-            filtering_exp = Key(filter_key).eq(filter_value)
-            response = self.table.query(KeyConditionExpression=filtering_exp)
-        else:
-            response = self.table.query()
-
-        return response["Items"]
-
-    def execute_query(
-        self,
-        key_expression=None,
-        filter_expression=None,
-        projection_expression=None,
-    ):
-        """
-        queries the db table
-        'key_expression' must be of type FilterExpression
-        'filter_expression' must be of type KeyConditionExpression
-        """
-        if filter_expression and not projection_expression:
-            response = self.table.query(
-                KeyConditionExpression=key_expression,
-                FilterExpression=filter_expression,
-            )
-        elif not filter_expression and projection_expression:
-            response = self.table.query(
-                KeyConditionExpression=key_expression,
-                ProjectionExpression=projection_expression,
-            )
-        elif filter_expression and projection_expression:
-            response = self.table.query(
-                KeyConditionExpression=key_expression,
-                FilterExpression=filter_expression,
-                ProjectionExpression=projection_expression,
-            )
-        else:
-            response = self.table.query(KeyConditionExpression=key_expression)
-
-        items = response["Items"]
-        while True:
-            if response.get("LastEvaluatedKey"):
-                response = self.table.scan(ExclusiveStartKey=response["LastEvaluatedKey"])
-                items += response["Items"]
-            else:
-                break
-
-        return items
-
-    def scan_firstpage(self, filter_key=None, filter_value=None):
-        """
-        Perform a scan operation on table.
-        Can specify filter_key (col name) and its value to be filtered.
-        This gets only first page of results in pagination. Returns the response.
-        """
-
-        if filter_key and filter_value:
-            filtering_exp = Attr(filter_key).is_in(filter_value)
-            response = self.table.scan(FilterExpression=filtering_exp)
-        else:
-            response = self.table.scan()
-
-        return response["Items"]
-
-    def scan_allpages(self, filter_key=None, filter_value=None):
-        """
-        Perform a scan operation on table.
-        Can specify filter_key (col name) and its value to be filtered.
-        This gets all pages of results.
-        Returns list of items.
-        """
-        filtering_exp=None
-        
-        if filter_key and filter_value:
-            filtering_exp = Attr(filter_key).is_in(filter_value)
-            response = self.table.scan(FilterExpression=filtering_exp)
-        else:
-            response = self.table.scan()
-
-        items = response["Items"]
-        while True:
-            if response.get("LastEvaluatedKey"):
-                response = self.table.scan(ExclusiveStartKey=response['LastEvaluatedKey'], FilterExpression=filtering_exp)
-                items += response["Items"]
-            else:
-                break
-
-        return items
-
-    def execute_scan(self, filter_expression, projection_expression=None):
-        """
-        scans the 'table_name'.
-        'filter_expression' must be of type FilterExpression
-        """
-        if projection_expression:
-            response = self.table.scan(
-                FilterExpression=filter_expression,
-                ProjectionExpression=projection_expression,
-            )
-        else:
-            response = self.table.scan(FilterExpression=filter_expression)
-
-        items = response["Items"]
-        while True:
-            if response.get("LastEvaluatedKey"):
-                response = self.table.scan(ExclusiveStartKey=response["LastEvaluatedKey"])
-                items += response["Items"]
-            else:
-                break
-
-        return items
-
-    def get_table(self):
-        return self.table
 

c82fa40f4a17fb0bad5ac0faa5dba2111d333938
Sanjiv Singh <sk.sanjiv@gmail.com>
2022-10-29 18:22:21 +0530

    add alert functionality test in main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ DEVICE_ID_1 = 'BSM_G101'
 DEVICE_ID_2 = 'BSM_G102'
 
 # Start and end timestamps based on the data generated
-START_TIMESTAMP = '2022-10-29 08:31:00.000000'
-END_TIMESTAMP = '2022-10-29 08:36:00.000000'
+START_TIMESTAMP = '2022-10-29 14:40:00.000000'
+END_TIMESTAMP = '2022-10-29 15:50:00.000000'
 
-RULES_CONFIG = '/rules.json'
+RULES_CONFIG = 'rules.json'
 
 
 raw_data_model = RawDataModel()
@@ -26,4 +26,5 @@ agg_data = raw_data_model.aggregate_by_datatype_and_minute(DEVICE_ID_2, START_TI
 aggregate_data_model.insert(DEVICE_ID_2, agg_data)
 
 # Processing the rules on both devices
-
+alert_data_model = AlertDataModel(RULES_CONFIG)
+alert_data_model.process_rules(DEVICE_ID_1, START_TIMESTAMP, END_TIMESTAMP)

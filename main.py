320ec778f9a02731e4b45c7cbcec60a32790ad39
Sanjiv Singh <sk.sanjiv@gmail.com>
2022-10-30 09:52:57 +0530

    modifide main to use parameters specified in config file

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
 from RawDataModel import RawDataModel
 from AggregateDataModel import AggregateDataModel
 from AlertDataModel import AlertDataModel
+import json
+
+
+def load_config(file):
+    with open(file) as f:
+        config = json.loads(f.read())
+    return config
+
+if __name__ == '__main__':
+
+    # Load config from file (json format)
+    config = load_config("config.json")
+
+    devices = config.get("devices")
+    start_timestamp = config.get("start_timestamp")
+    end_timestamp = config.get("end_timestamp")
+
+    # Create the raw data model instances for raw, aggregate and alerts data
+    raw_data_model = RawDataModel()
+    aggregate_data_model = AggregateDataModel()
+    alert_data_model = AlertDataModel(config.get("rules_file"))
+
+    # Aggregating items from raw model and then inserting them in the aggregate model
+    for device in devices:
+        device_id = device.get("device_id")
+        print(f'Aggregating data for device {device_id}')
+        # Get aggregated data
+        agg_data = raw_data_model.aggregate_by_datatype_and_minute(device_id, start_timestamp, end_timestamp)
+        # Insert into agg_data table
+        aggregate_data_model.insert(device_id, agg_data)
+
+    # Processing the rules on both devices
+    for device in devices:
+        device_id = device.get("device_id")
+        print(f'Processing rules for device {device_id}')
+        alert_data_model.process_rules(device_id, start_timestamp, end_timestamp)
 
 
 # Two device ids
@@ -14,17 +50,3 @@ END_TIMESTAMP = '2022-10-29 15:50:00.000000'
 RULES_CONFIG = 'rules.json'
 
 
-raw_data_model = RawDataModel()
-aggregate_data_model = AggregateDataModel()
-alert_data_model = AlertDataModel(RULES_CONFIG)
-
-# Aggregating items from raw model and then inserting them in the aggregate model
-agg_data = raw_data_model.aggregate_by_datatype_and_minute(DEVICE_ID_1, START_TIMESTAMP, END_TIMESTAMP)
-aggregate_data_model.insert(DEVICE_ID_1, agg_data)
-
-agg_data = raw_data_model.aggregate_by_datatype_and_minute(DEVICE_ID_2, START_TIMESTAMP, END_TIMESTAMP)
-aggregate_data_model.insert(DEVICE_ID_2, agg_data)
-
-# Processing the rules on both devices
-alert_data_model = AlertDataModel(RULES_CONFIG)
-alert_data_model.process_rules(DEVICE_ID_1, START_TIMESTAMP, END_TIMESTAMP)
